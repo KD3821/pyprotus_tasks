c0e4278587d43e199fde7fa294e03d824a29c7d8
KD3821 <80853241+KD3821@users.noreply.github.com>
2023-12-15 05:11:35 +0300

    refactor: improve 'process_lines' func & add logging

--- advanced_basics/log_analyzer.py
+++ advanced_basics/log_analyzer.py
@@ -12,12 +12,15 @@ import fnmatch
 import gzip
 import re
 import sys
+from statistics import median
+import logging
 
 
 config = {
     "REPORT_SIZE": 1000,
     "REPORT_DIR": "./reports",
-    "LOG_DIR": "./log"
+    "LOG_DIR": "./log",
+    "LOG_FILE": "log_analyzer.log"
 }
 
 
@@ -25,7 +28,19 @@ def get_config():
     return config
 
 
+logging.basicConfig(
+        level=logging.ERROR,
+        filename=get_config().get('LOG_FILE'),
+        filemode='w',
+        format='[%(asctime)s] %(levelname).1s %(message)s',
+        datefmt='%Y.%m.%d %H:%M:%S',
+    )
+
+
 def find_log_file(file_name_pattern, dir_name):
+    """
+    Поиск в указанной директории файлов с именем, соответствующим шаблону, и их передача в дальнейшую обработку
+    """
     with os.scandir(path=dir_name) as it:
         for entry in it:
             if fnmatch.fnmatch(entry.name, file_name_pattern):
@@ -33,6 +48,10 @@ def find_log_file(file_name_pattern, dir_name):
 
 
 def find_latest_date(log_file, recent_date):
+    """
+    Сравнение даты файла с самой поздней датой ранее проверенного файла. Если дата текущего файла новее, возвращается
+    его дата и имя, чтобы далее проводить сравнение данных остальных файлов с ними
+    """
     file_date = int(log_file.name.split('.')[1].split('-')[-1])
 
     if recent_date is None:
@@ -45,6 +64,9 @@ def find_latest_date(log_file, recent_date):
 
 
 def get_recent_log_file(file_iterator):
+    """
+    Определяем файл с наиболее новой датой
+    """
     recent_date = None
     file_name = None
     while True:
@@ -59,6 +81,9 @@ def get_recent_log_file(file_iterator):
 
 
 def check_report_exist(report_date, report_dir_name):
+    """
+    Проверяем наличие ранее созданного отчета по файлу с самой новой датой. Если в наличии - останавливаем обработку
+    """
     report_file_pattern = 'report' + '-' + str(report_date)[:4] + '.' + str(report_date)[4:6] + '.' \
                           + str(report_date)[-2:] + '.html'
 
@@ -69,6 +94,9 @@ def check_report_exist(report_date, report_dir_name):
 
 
 def process_logs(file_name, log_dir_name):
+    """
+    Открываем файл и передаем в дальнейшую обработку построчно
+    """
     if file_name.endswith(".gz"):
         log = gzip.open(os.path.join(log_dir_name, file_name), 'rt')
     else:
@@ -80,68 +108,95 @@ def process_logs(file_name, log_dir_name):
     log.close()
 
 
-def process_lines(line_iterator):
+def get_url_time(x):
+    """
+    Вспомогательная функция для сортировки списка словарей по указанному ключу ('time_sum')
+    """
+    return x.get('time_sum')
+
+
+def process_lines(line_iterator, file_name, log_dir_name, report_size):
+    """
+    Основная обработка логов.
+    """
 
     total_count = 0
     total_time = 0.0
-    urls_data = {}
+    urls_done = []
+    urls_data_list = []
+
+    first_time = True
 
     while True:
         try:
             line = next(line_iterator)
-            log_list = line.split()
-            url = log_list[6]
-            t = float(log_list[-1])
-            total_count += 1
-            total_time += t
-            first_time = True
-            for key, value in urls_data.items():
-                if key == url:
-                    value['count'] = value.get('count') + 1
-                    value['time'] = value.get('time') + t
-                    value['med'].append(t)
-                    if t > value.get('max'):
-                        value['max'] = t
-                    first_time = False
+            line_list = line.split()
+            line_url = line_list[6]
+
+            if line_url in urls_done:
+                continue
+
+            line_count = 0
+            line_time_sum = 0.0
+            line_time_list = []
+
+            log_iterator = process_logs(file_name, log_dir_name)
+
+            while True:
+                try:
+                    log = next(log_iterator)
+                    log_list = log.split()
+                    log_url = log_list[6]
+                    log_t = float(log_list[-1])
+
+                    if first_time:
+                        total_count += 1
+                        total_time += log_t
+
+                    if log_url == line_url:
+                        line_count += 1
+                        line_time_sum += log_t
+                        line_time_list.append(log_t)
+
+                except StopIteration:
+                    if first_time:
+                        first_time = False
                     break
-            if first_time:
-                urls_data[url] = dict(count=1, time=t, max=t, med=[t])
+
+            line_count_perc = line_count / total_count * 100
+            line_time_sum_perc = line_time_sum / total_time * 100
+            line_time_avg = line_time_sum / line_count
+            line_time_max = max(line_time_list)
+            line_time_median = median(line_time_list)
+
+            urls_done.append(line_url)
+
+            urls_data_list.append({
+                'url': line_url,
+                'count': line_count,
+                'count_perc': line_count_perc,
+                'time_sum': line_time_sum,
+                'time_perc': line_time_sum_perc,
+                'time_avg': line_time_avg,
+                'time_max': line_time_max,
+                'time_med': line_time_median
+            })
 
         except StopIteration:
             break
-    return urls_data, total_count, total_time
 
+    urls_data_list.sort(reverse=True, key=get_url_time)
 
-def finalize_report(urls_data, total_count, total_time, report_size):
-    final_data = []
+    if len(urls_data_list) >= report_size:
+        return urls_data_list[0:report_size]
 
-    for key, value in urls_data.items():   # TODO use report_size to take first 1000 with max time_sum:
-        tmp = value.get('med')
-
-        if len(tmp) % 2 != 0:
-            med = len(tmp) // 2
-            time_med = tmp[med]
-        else:
-            med_1 = len(tmp) // 2
-            med_0 = med_1 - 1
-            time_med = (tmp[med_0] + tmp[med_1]) / 2
-
-        log_stat = dict(
-            url=key,
-            count=value.get('count'),
-            time_avg=value.get('time') / value.get('count'),
-            time_max=value.get('max'),
-            time_sum=value.get('time'),
-            time_med=time_med,
-            time_perc=(value.get('time') / total_time) * 100,
-            count_perc=(value.get('count') / total_count) * 100
-        )
-
-        final_data.append(log_stat)
-    return final_data
+    return urls_data_list
 
 
 def create_report_file(final_data, file_date, dir_path):
+    """
+    Создаем файл и записываем текст шаблона + обработанные данные логов
+    """
 
     report_file_name = 'report' + '-' + str(file_date)[:4] + '.' + str(file_date)[4:6] + '.' + str(file_date)[-2:] \
                        + '.html'
@@ -165,7 +220,7 @@ def create_report_file(final_data, file_date, dir_path):
             else:
                 data += i
 
-        with open(file_path, 'w') as f:
+        with open(file_path, 'w', encoding='utf-8') as f:
             f.write(data)
 
 
@@ -174,11 +229,11 @@ def main():
     gen_log_file = find_log_file("nginx-access-ui.log*", actual_conf.get("LOG_DIR"))
     log_file_name, report_date = get_recent_log_file(gen_log_file)
     if check_report_exist(report_date, actual_conf.get("REPORT_DIR")):
+        logging.error('Отчет за указанную дату уже создан.')
         sys.exit()
     gen_lines = process_logs(log_file_name, actual_conf.get("LOG_DIR"))
-    data, count, time = process_lines(gen_lines)
-    final = finalize_report(data, count, time, actual_conf.get("REPORT_SIZE"))
-    create_report_file(final, report_date, actual_conf.get("REPORT_DIR"))
+    urls = process_lines(gen_lines, log_file_name, actual_conf.get("LOG_DIR"), actual_conf.get("REPORT_SIZE"))
+    create_report_file(urls, report_date, actual_conf.get("REPORT_DIR"))
 
 
 if __name__ == "__main__":
