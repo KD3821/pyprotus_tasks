bbf84c1d041c7e4c41a9f3285283f403a91ac511
KD3821 <80853241+KD3821@users.noreply.github.com>
2024-01-02 23:57:35 +0300

    feat: add base logic for API

---
+++ oop/api.py
@@ -0,0 +1,257 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import abc
+import json
+import datetime
+import logging
+import hashlib
+import uuid
+from optparse import OptionParser
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+SALT = "Otus"
+ADMIN_LOGIN = "admin"
+ADMIN_SALT = "42"
+OK = 200
+BAD_REQUEST = 400
+FORBIDDEN = 403
+NOT_FOUND = 404
+INVALID_REQUEST = 422
+INTERNAL_ERROR = 500
+ERRORS = {
+    BAD_REQUEST: "Bad Request",
+    FORBIDDEN: "Forbidden",
+    NOT_FOUND: "Not Found",
+    INVALID_REQUEST: "Invalid Request",
+    INTERNAL_ERROR: "Internal Server Error",
+}
+UNKNOWN = 0
+MALE = 1
+FEMALE = 2
+GENDERS = {
+    UNKNOWN: "unknown",
+    MALE: "male",
+    FEMALE: "female",
+}
+
+
+class CharField(object):
+    def __init__(self, required: bool = False, nullable: bool = True):
+        self.required = required
+        self.nullable = nullable
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name, '')
+
+    def __set__(self, instance, value):
+        setattr(instance, self.name, value)
+
+    @classmethod
+    def get_descriptor_method(cls, name):
+        try:
+            method = getattr(cls, '__'+name+'__')
+        except AttributeError as e:
+            raise Exception(f'Недоступный метод: {e}')
+        return method
+
+
+class ArgumentsField(object):
+    def __init__(self, arg_data: dict = {}, required: bool = True, nullable: bool = True):
+        self.arg_data = arg_data
+        self.required = required
+        self.nullable = nullable
+
+
+class EmailField(CharField):
+    def __set__(self, instance, value):
+        self.verify_email(value)
+        setattr(instance, self.name, value)
+
+    @staticmethod
+    def verify_email(email):
+        if email.find('@') != -1:
+            raise ValueError("Ошибка валидации: email должен содержать '@' символ.")
+        elif email.count() > 1:
+            raise ValueError("Ошибка валидации: некорректный email")
+
+
+class PhoneField(object):
+    def __init__(self, required: bool = False, nullable: bool = True):
+        self.required = required
+        self.nullable = nullable
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name, '')
+
+    def __set__(self, instance, value):
+        self.verify_number(value)
+        setattr(instance, self.name, value)
+
+    @staticmethod
+    def verify_number(value):
+        number = str(value)
+        if number[0] != '7':
+            raise ValueError("Некорректный номер телефона: первая цифра должна быть '7'.")
+        elif len(number) != 11:
+            raise ValueError("Некорректный номер телефона: должен содержать 11 символов.")
+        elif not number.isnumeric():
+            raise ValueError("Некорректный номер телефона: должен состоять из цифр.")
+
+
+class DateField(object):
+    def __init__(self, date: str = '', required: bool = False, nullable: bool = True):
+        self.date = date
+        self.required = required
+        self.nullable = nullable
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
+    def __get__(self, instance, owner):
+        date = getattr(instance, self.name, '')
+        if date != '':
+            return self.validate_format(date)
+
+    def __set__(self, instance, value):
+        validated_value = self.validate_format(value)
+        setattr(instance, self.name, validated_value)
+
+    @staticmethod
+    def validate_format(date):
+        try:
+            formatted_date = datetime.datetime.strptime(date, "%d.%m.%Y")
+        except ValueError as e:
+            raise Exception(f'Неверный формат даты: {e}')
+        return formatted_date
+
+
+class BirthDayField(object):
+    def __init__(self, date: str = '', required: bool = False, nullable: bool = True):
+        self.date = date
+        self.required = required
+        self.nullable = nullable
+
+    @staticmethod
+    def validate_format(date):
+        try:
+            formatted_date = datetime.datetime.strptime(date, "%d.%m.%Y")
+            if formatted_date > datetime.datetime(1970, 1, 1):
+                return formatted_date
+        except ValueError as e:
+            raise Exception(f'Неверный формат даты: {e}')
+
+
+class GenderField(object):
+    pass
+
+
+class ClientIDsField(object):
+    pass
+
+
+class ClientsInterestsRequest(object):
+    client_ids = ClientIDsField(required=True)
+    date = DateField(required=False, nullable=True)
+
+
+class OnlineScoreRequest(object):
+    first_name = CharField(required=False, nullable=True)
+    last_name = CharField(required=False, nullable=True)
+    email = EmailField(required=False, nullable=True)
+    phone = PhoneField(required=False, nullable=True)
+    birthday = BirthDayField(required=False, nullable=True)
+    gender = GenderField(required=False, nullable=True)
+
+
+class MethodRequest(object):
+    account = CharField(required=False, nullable=True)
+    login = CharField(required=True, nullable=True)
+    token = CharField(required=True, nullable=True)
+    arguments = ArgumentsField(required=True, nullable=True)
+    method = CharField(required=True, nullable=False)
+
+    @property
+    def is_admin(self):
+        return self.login == ADMIN_LOGIN
+
+
+def check_auth(request):
+    if request.is_admin:
+        digest = hashlib.sha512(datetime.datetime.now().strftime("%Y%m%d%H") + ADMIN_SALT).hexdigest()
+    else:
+        digest = hashlib.sha512(request.account + request.login + SALT).hexdigest()
+    if digest == request.token:
+        return True
+    return False
+
+
+def method_handler(request, ctx, store):
+    response, code = None, None
+    return response, code
+
+
+class MainHTTPHandler(BaseHTTPRequestHandler):
+    router = {
+        "method": method_handler
+    }
+    store = None
+
+    def get_request_id(self, headers):
+        return headers.get('HTTP_X_REQUEST_ID', uuid.uuid4().hex)
+
+    def do_POST(self):
+        response, code = {}, OK
+        context = {"request_id": self.get_request_id(self.headers)}
+        request = None
+        try:
+            data_string = self.rfile.read(int(self.headers['Content-Length']))
+            request = json.loads(data_string)
+        except:
+            code = BAD_REQUEST
+
+        if request:
+            path = self.path.strip("/")
+            logging.info("%s: %s %s" % (self.path, data_string, context["request_id"]))
+            if path in self.router:
+                try:
+                    response, code = self.router[path]({"body": request, "headers": self.headers}, context, self.store)
+                except Exception as e:
+                    logging.exception("Unexpected error: %s" % e)
+                    code = INTERNAL_ERROR
+            else:
+                code = NOT_FOUND
+
+        self.send_response(code)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+        if code not in ERRORS:
+            r = {"response": response, "code": code}
+        else:
+            r = {"error": response or ERRORS.get(code, "Unknown Error"), "code": code}
+        context.update(r)
+        logging.info(context)
+        self.wfile.write(json.dumps(r))
+        return
+
+
+if __name__ == "__main__":
+    op = OptionParser()
+    op.add_option("-p", "--port", action="store", type=int, default=8080)
+    op.add_option("-l", "--log", action="store", default=None)
+    (opts, args) = op.parse_args()
+    logging.basicConfig(filename=opts.log, level=logging.INFO,
+                        format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
+    server = HTTPServer(("localhost", opts.port), MainHTTPHandler)
+    logging.info("Starting server at %s" % opts.port)
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    server.server_close()
